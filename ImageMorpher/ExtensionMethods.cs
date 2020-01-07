bc7bb956f97f8358a1735780228545203f0757d5
Daniel Hodek <dhodek33@gmail.com>
2020-01-07 10:46:35 -0800

    Initial commit

---
+++ ImageMorpher/ExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace ExtensionMethods
+{
+    public static class CanvasExtensions
+    {
+        public static void DrawLine(this Canvas canvas, Brush color, double thickness, double x1, double y1, double x2, double y2)
+        {
+            Line line = new Line();
+            line.Stroke = color;
+            line.StrokeThickness = thickness;
+            line.X1 = x1;
+            line.Y1 = y1;
+            line.X2 = x2;
+            line.Y2 = y2;
+            canvas.Children.Add(line);
+        }
+    }
+}
---
+++ ImageMorpher/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ImageMorpher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private OpenFileDialog openFileDialog;
+        private List<Line> controlLines;
+        private Point startPoint;
+        private Point endPoint;
+        private bool isStartPoint;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            openFileDialog = new OpenFileDialog();
+            isStartPoint = true;
+        }
+
+        private void SelectSourceButton_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)openFileDialog.ShowDialog())
+            {
+                try
+                {
+                    string fileName = openFileDialog.FileName;
+                    sourceImage.Source = new BitmapImage(new Uri(fileName));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        private void SourceCanvas_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Console.WriteLine("Clicked");
+            if (isStartPoint)
+            {
+                startPoint = Mouse.GetPosition(sourceCanvas);
+                isStartPoint = false;
+            }
+            else
+            {
+                endPoint = Mouse.GetPosition(sourceCanvas);
+                sourceCanvas.DrawLine(Brushes.Red, 2, )
+            }
+        }
+    }
+}
